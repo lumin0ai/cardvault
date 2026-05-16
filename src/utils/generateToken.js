f3f3090be8d35878869f0cdba48b098d7e9a05ca
satyam <satyam@navigolabs.com>
2026-05-16 23:19:15 +0530

    fix: timing of token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || "30d",
+  });
 };
 
 export const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRE || "90d",
+  });
 };
