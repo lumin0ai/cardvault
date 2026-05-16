b500675ffcf0084ec66fbb1b06d2bd8bd26f0faa
satyam <satyam@navigolabs.com>
2026-05-16 16:30:40 +0530

    feat: cenetrlizied try catch for export

--- src/modules/export/export.controller.js
+++ src/modules/export/export.controller.js
@@ -1,19 +1,29 @@
+import asyncHandler from "../../utils/asyncHandler.js";
 import { exportFolderService } from "./export.service.js";
 
-export const exportFolder = async (req, res) => {
-  try {
-    const fileName = await exportFolderService(
-      req.params.folderId,
-      req.user._id,
-    );
-    const fileUrl = `${req.protocol}://${req.get("host")}/exports/${fileName}`;
-    res.status(200).json({
-      message: "Export successfully",
-      fileUrl,
-    });
-  } catch (error) {
-    res.status(400).json({
-      message: error.message,
-    });
-  }
-};
+export const exportFolder = asyncHandler(async (req, res) => {
+  const fileName = await exportFolderService(req.params.folderId, req.user._id);
+  const fileUrl = `${req.protocol}://${req.get("host")}/exports/${fileName}`;
+  res.status(200).json({
+    message: "Export successfully",
+    fileUrl,
+  });
+});
+
+// async (req, res) => {
+//   try {
+//     const fileName = await exportFolderService(
+//       req.params.folderId,
+//       req.user._id,
+//     );
+//     const fileUrl = `${req.protocol}://${req.get("host")}/exports/${fileName}`;
+//     res.status(200).json({
+//       message: "Export successfully",
+//       fileUrl,
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       message: error.message,
+//     });
+//   }
+// };
