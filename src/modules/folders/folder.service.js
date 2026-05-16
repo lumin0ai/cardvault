292248f73063b9eb56888ed911a0de9511949cf0
satyam <satyam@navigolabs.com>
2026-05-17 01:16:12 +0530

    feat: added a count of contacts in each folder

--- src/modules/folders/folder.service.js
+++ src/modules/folders/folder.service.js
@@ -1,4 +1,5 @@
 import Folder from "./folder.model.js";
+import mongoose from "mongoose";
 
 export const createFolderService = async (userId, data) => {
   return await Folder.create({
@@ -8,9 +9,44 @@ export const createFolderService = async (userId, data) => {
 };
 
 export const getFoldersService = async (userId) => {
-  return await Folder.find({ userId }).sort({
-    createdAt: -1,
-  });
+  // return await Folder.find({ userId }).sort({
+  //   createdAt: -1,
+  // });
+  return await Folder.aggregate([
+    {
+      $match: {
+        userId:
+          new mongoose.Types.ObjectId(
+            userId
+          ),
+      },
+    },
+    {
+      $lookup: {
+        from: 'contacts',
+        localField: '_id',
+        foreignField: 'folderId',
+        as: 'contacts'
+      }
+    },
+    {
+      $addFields: {
+        contactsCount: {
+          $size: '$contacts',
+        }
+      }
+    },
+    {
+      $project: {
+        contacts: 0,
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ]);
 };
 
 export const updateFolderService = async (folderId, userId, data) => {
